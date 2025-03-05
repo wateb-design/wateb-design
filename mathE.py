b91c607268b888cb41684d31165576b555f189fb
WAFFO EZECHIEL <watebvision@gmail.com>
2025-03-05 15:23:00 +0100

    Update mathE.py

--- mathE.py
+++ mathE.py
@@ -12,3 +12,35 @@ def load_data():
 datafile = load_data()
 
 st.write('hello')
+
+#INTERFACE DE SAISIE DES DONNEES
+
+st.sidebar.title("Prédiction")
+menu = st.sidebar.radio("Sélectionner une option", ["ACCEUIL", "PREDICTION", "A PROPOS DE NOUS"])
+if menu == "ACCEUIL":
+    st.title("ACCEUIL")
+    st.write("Bienvenue sur notre API de prédiction des especes de fleurs entrainée avec le jeu de donées <<Iris_Dataset.csv>>")
+    st.write("")
+    st.write("Voici un aperçu du jeu de données Iris (les 05 premières lignes) :")
+    st.dataframe(df.head()) 
+    st.write("")
+    st.write("Rendez-vous au menu préddiction, entrer les dimension que vous voulez, et notre plateforme se chargera de vous prédire de quelle espèces cette fleurs appatiens.")
+    
+# Formulaire pour saisir les dimensions
+elif menu == "PREDICTION":
+    st.title("PREDICTION DES FLEURS")
+    # Affichage de la précision du modèle
+    st.write(f"Précision du modèle : {accuracy:.2f}")
+    st.write("Entrez les dimensions de la fleur :")
+
+# Prédiction du type de fleur
+    if st.button("Prédire"):
+        # Créer un DataFrame à partir des entrées de l'utilisateur
+        user_input = pd.DataFrame([[sepal_length, sepal_width, petal_length, petal_width]], 
+                              columns=["SepalLength", "SepalWidth", "PetalLength", "PetalWidth"])
+    # Prédire le type de fleur
+     
+
+elif menu == "A PROPOS DE NOUS":
+    st.write("Etudiant à l'ENS de Yaoundé, filière informatique")
+    st.write("@Waffo")
