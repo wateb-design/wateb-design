873e5ada9f429f586f7cb6b32c629eb8e12a82f8
WAFFO EZECHIEL <watebvision@gmail.com>
2025-03-05 15:13:05 +0100

    Update mathE.py

--- mathE.py
+++ mathE.py
@@ -4,3 +4,4 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pickle
+st.write('hello')
