1d7afe161dc64cde053f7fca9a24a1cf95cfdae4
WAFFO EZECHIEL <watebvision@gmail.com>
2025-03-05 15:15:49 +0100

    Update mathE.py

--- mathE.py
+++ mathE.py
@@ -4,4 +4,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pickle
+
+def load_data():
+    # Charger le fichier CSV localement
+    datafile=pd.read_csv('Iris.csv', delimiter=";")
+    return df
+datafile = load_data()
+
 st.write('hello')
