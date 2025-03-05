855020205dfbe612fc70d5b8e0cd93d8859ce196
WAFFO EZECHIEL <watebvision@gmail.com>
2025-03-05 15:01:43 +0100

    Update mathE.py

--- mathE.py
+++ mathE.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-import pickle
 
 # Charger le fichier CSV du jeu de données Iris
 #@st.cache
