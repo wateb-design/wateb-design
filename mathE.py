99da598abc808c25674409b6ae5f29ff77d5ba64
WAFFO EZECHIEL <watebvision@gmail.com>
2025-03-05 16:09:41 +0100

    Update mathE.py

--- mathE.py
+++ mathE.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
