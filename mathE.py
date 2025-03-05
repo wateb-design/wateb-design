b4a1a4dca8702fd3a813b4782d72cdafed1994b0
WAFFO EZECHIEL <watebvision@gmail.com>
2025-03-05 15:05:32 +0100

    Update mathE.py

--- mathE.py
+++ mathE.py
@@ -3,10 +3,5 @@ import pandas as pd
 
 # Charger le fichier CSV du jeu de données Iris
 #@st.cache
-"""def load_data():
-    # Charger le fichier CSV localement
-    datafile=pd.read_csv('MathE_data.csv', delimiter=";", encoding='cp1252')
-return df
-datafile = load_data()"""
 
 st.write('hello')
